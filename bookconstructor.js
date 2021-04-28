491fa1d68e8779a1cbf50b0894253230e6ad99e7
paposeco <13892562+paposeco@users.noreply.github.com>
2021-04-28 17:46:13 +0200

    add and remove book features implemented

--- bookconstructor.js
+++ bookconstructor.js
@@ -1,8 +1,8 @@
 function bookInfo(title, author, pages, status) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
-  this.status = status;
+  this.pages = "Pages: " + pages;
+  this.status = "Status: " + status;
   this.info = function () {
     if (status === "read") {
       return `${title} by ${author}, ${pages} pages, ${status}`;
@@ -26,24 +26,57 @@ let bookCollection = [hobbit, seveneves, tombombadil, hyperion];
 
 function addBookToLibrary(title, author, pages, status) {
   let newBook = new bookInfo(title, author, pages, status);
-  return bookCollection.push(newBook);
+  bookCollection.push(newBook);
+  return bookCollection;
 }
 
 const collection = document.getElementById("collection");
 
-function displayCollection() {
-  for (i = 0; i < bookCollection.length; i++) {
+function displayCollection(array) {
+  for (i = 0; i < array.length; i++) {
     const newUl = document.createElement("ul");
+    newUl.setAttribute("data-attribute", "book" + i);
     collection.appendChild(newUl);
-    let bookTitle = bookCollection[i].title;
-    let bookAuthor = bookCollection[i].author;
-    let bookPages = bookCollection[i].pages;
-    let bookStatus = bookCollection[i].status;
+    let bookTitle = array[i].title;
+    let bookAuthor = array[i].author;
+    let bookPages = array[i].pages;
+    let bookStatus = array[i].status;
     createLi(newUl, bookTitle);
     createLi(newUl, bookAuthor);
     createLi(newUl, bookPages);
     createLi(newUl, bookStatus);
+    removeBookButton(i, newUl);
   }
+}
+
+function addToCollection() {
+  const newUl = document.createElement("ul");
+  newUl.setAttribute("data-attribute", "book" + (bookCollection.length - 1));
+  collection.appendChild(newUl);
+  let bookTitle = bookCollection[bookCollection.length - 1].title;
+  let bookAuthor = bookCollection[bookCollection.length - 1].author;
+  let bookPages = bookCollection[bookCollection.length - 1].pages;
+  let bookStatus = bookCollection[bookCollection.length - 1].status;
+  createLi(newUl, bookTitle);
+  createLi(newUl, bookAuthor);
+  createLi(newUl, bookPages);
+  createLi(newUl, bookStatus);
+  removeBookButton(bookCollection.length - 1, newUl);
+}
+
+function removeBookButton(index, newUl) {
+  const newButton = document.createElement("button");
+  newButton.textContent = "Remove Book";
+  let currentID = "book" + index;
+  newButton.setAttribute("id", currentID);
+  newButton.setAttribute("name", currentID);
+  newButton.setAttribute("class", "bookRemoval");
+  newUl.appendChild(newButton);
+  newButton.addEventListener("click", function () {
+    bookCollection.splice(index, 1);
+    collection.innerHTML = "";
+    displayCollection(bookCollection);
+  });
 }
 
 function createLi(newUl, information) {
@@ -51,6 +84,50 @@ function createLi(newUl, information) {
   newListItem.textContent = information;
   return newUl.appendChild(newListItem);
 }
+window.onload = displayCollection(bookCollection);
 
-//collection.appendChild(list);
-displayCollection();
+const formDiv = document.getElementById("formdiv");
+const showFormButton = document.getElementById("bringform");
+const showFormDiv = document.getElementById("bringformbutton");
+const addbookButton = document.getElementById("addbookbutton");
+
+const inputTitle = document.getElementById("title");
+const inputAuthor = document.getElementById("author");
+const inputPages = document.getElementById("pages");
+const inputStatus = document.getElementById("status");
+const form = document.getElementById("newbook");
+const allInputs = document.querySelectorAll("input");
+const closeForm = document.getElementById("closeform");
+const bookRemoval = document.querySelectorAll(".bookRemoval");
+
+showFormButton.addEventListener("click", function (event) {
+  formDiv.style.visibility = "visible";
+  showFormDiv.style.visibility = "hidden";
+});
+
+addbookButton.addEventListener("click", function (event) {
+  let bookTitle = inputTitle.value;
+  let bookAuthor = inputAuthor.value;
+  let bookPages = inputPages.value;
+  let bookStatus = inputStatus.value;
+  addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus);
+  addToCollection();
+  formDiv.style.visibility = "hidden";
+  showFormDiv.style.visibility = "visible";
+  form.reset();
+  event.preventDefault(); //it would refresh after submitting without this
+});
+
+closeForm.addEventListener("click", function (event) {
+  form.reset();
+  formDiv.style.visibility = "hidden";
+  showFormDiv.style.visibility = "visible";
+});
+
+// bookRemoval.forEach(function (button) {
+//   button.addEventListener("click", function (target) {
+//     let buttonID = target.id;
+//     let arrayIndex = buttonID.split(5);
+//     console.log(arrayIndex);
+//   });
+// });
