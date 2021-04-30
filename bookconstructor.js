74b6feb6296f870db4cf6c5cf1839dac5ba2fed6
paposeco <13892562+paposeco@users.noreply.github.com>
2021-04-30 15:24:19 +0200

    styling

--- bookconstructor.js
+++ bookconstructor.js
@@ -1,6 +1,6 @@
 function bookInfo(title, author, pages, status) {
   this.title = title;
-  this.author = author;
+  this.author = "Author: " + author;
   this.pages = "Pages: " + pages;
   this.status = "Status: " + status;
   this.info = function () {
@@ -12,24 +12,56 @@ function bookInfo(title, author, pages, status) {
   };
 }
 
-const hobbit = new bookInfo("The Hobbit", "J.R.R. Tolkien", "295", "read");
+const hobbit = new bookInfo("The Hobbit", "J.R.R. Tolkien", "295", "not read");
 const seveneves = new bookInfo("Seveneves", " Neal Stephenson", "880", "read");
 const tombombadil = new bookInfo(
-  "The Adventures of Tom Bombadil ",
+  "The Adventures of Tom Bombadil",
   "J.R.R. Tolkien",
   "75",
   "not read"
 );
 const hyperion = new bookInfo("Hyperion", "Dan Simmons", "482", "read");
 
-let bookCollection = [hobbit, seveneves, tombombadil, hyperion];
+let bookCollection = [
+  hobbit,
+  seveneves,
+  tombombadil,
+  hyperion,
+  hobbit,
+  seveneves,
+  tombombadil,
+  hyperion,
+  hobbit,
+  seveneves,
+  tombombadil,
+  hyperion,
+  hobbit,
+  seveneves,
+  tombombadil,
+  hyperion,
+  tombombadil,
+  hyperion,
+  hobbit,
+  seveneves,
+  tombombadil,
+  hyperion,
+  tombombadil,
+  hyperion,
+  hobbit,
+  seveneves,
+  tombombadil,
+  hyperion,
+];
 const collection = document.getElementById("collection");
 
 function displayCollection(array) {
   for (i = 0; i < array.length; i++) {
+    const newDivOut = document.createElement("div");
+    newDivOut.classList.add("book");
+    collection.appendChild(newDivOut);
     const newUl = document.createElement("ul");
     newUl.setAttribute("id", "book" + i);
-    collection.appendChild(newUl);
+    newDivOut.appendChild(newUl);
     let bookTitle = array[i].title;
     let bookAuthor = array[i].author;
     let bookPages = array[i].pages;
@@ -38,8 +70,10 @@ function displayCollection(array) {
     createLi(newUl, bookAuthor, i, "bookauthor");
     createLi(newUl, bookPages, i, "bookpages");
     createLi(newUl, bookStatus, i, "bookstatus");
-    removeBookButton(i, newUl);
-    changeStatusButton(i, newUl, bookStatus);
+    const newDiv = document.createElement("div");
+    newUl.appendChild(newDiv);
+    removeBookButton(i, newDiv);
+    changeStatusButton(i, newDiv, bookStatus);
     populateStorage(array, i);
   }
 }
@@ -48,33 +82,57 @@ function addBookToCollection(title, author, pages, status, booksource) {
   let newBook = new bookInfo(title, author, pages, status);
   bookCollection.push(newBook);
   let currentIndex = bookCollection.length - 1;
+  const newDivOut = document.createElement("div");
+  newDivOut.classList.add("book");
+  collection.appendChild(newDivOut);
   const newUl = document.createElement("ul");
   newUl.setAttribute("id", "book" + currentIndex);
-  collection.appendChild(newUl);
-  createLi(newUl, newBook.title, currentIndex, "booktitle");
+  newDivOut.appendChild(newUl);
+  const liTitle = createLi(newUl, newBook.title, currentIndex, "booktitle");
+  const span = document.createElement("span");
   createLi(newUl, newBook.author, currentIndex, "bookauthor");
   createLi(newUl, newBook.pages, currentIndex, "bookpages");
   createLi(newUl, newBook.status, currentIndex, "bookstatus");
-  removeBookButton(currentIndex, newUl);
-  changeStatusButton(currentIndex, newUl, newBook.status);
+  if (newBook.status.slice(8) === "read") {
+    span.classList.add("statusRead");
+  } else {
+    span.classList.add("statusNot");
+  }
+  span.textContent = newBook.status.slice(8);
+  liTitle.appendChild(span);
+  const newDiv = document.createElement("div");
+  newUl.appendChild(newDiv);
+  removeBookButton(currentIndex, newDiv);
+  changeStatusButton(currentIndex, newDiv, newBook.status);
+  // toggles visibility
+  liTitle.addEventListener("click", function () {
+    const li = newUl.childNodes;
+    li.forEach(function (element) {
+      let classes = element.classList;
+      classes.toggle("visibleClass");
+    });
+  });
   if (booksource === "newbook") {
     populateStorage(bookCollection, currentIndex);
   }
 }
 
-function removeBookButton(index, newUl) {
+function removeBookButton(index, newDiv) {
   const newButton = document.createElement("button");
-  newButton.textContent = "Remove Book";
+  newButton.textContent = "X";
   let currentID = "bookremoval" + index;
   newButton.setAttribute("id", currentID);
   newButton.setAttribute("name", currentID);
+  newButton.setAttribute("title", "Remove Book");
   newButton.setAttribute("class", "bookRemoval");
-  newUl.appendChild(newButton);
+  newDiv.appendChild(newButton);
   newButton.addEventListener("click", function () {
     bookCollection.splice(index, 1);
     collection.innerHTML = "";
     displayCollection(bookCollection);
     removeItemLocalStorage(index);
+    const titleLi = document.querySelectorAll("[data-booktitle]");
+    hideLi(titleLi);
   });
 }
 
@@ -92,7 +150,7 @@ function createLi(newUl, information, i, typelististem) {
   return newUl.appendChild(newListItem);
 }
 
-function changeStatusButton(index, newUl, bookstatus) {
+function changeStatusButton(index, newDiv, bookstatus) {
   const newButton = document.createElement("button");
   let currentID = "markasread" + index;
   newButton.setAttribute("id", currentID);
@@ -103,7 +161,7 @@ function changeStatusButton(index, newUl, bookstatus) {
   } else {
     newButton.textContent = "Mark as not read";
   }
-  newUl.appendChild(newButton);
+  newDiv.appendChild(newButton);
   newButton.addEventListener("click", function (event) {
     changeStatus(event, index);
   });
@@ -117,8 +175,12 @@ function changeStatus(event, index) {
     const currentBookListItemStatus = currentBookList.querySelector(
       "[data-bookstatus]"
     );
+    const span = currentBookList.querySelector("span");
     currentBookListItemStatus.textContent = "Status: read";
     event.target.textContent = "Mark as not read";
+    span.textContent = "read";
+    span.classList.remove("statusNot");
+    span.classList.add("statusRead");
     localStorage.setItem("status" + index, "read");
   } else {
     bookCollection[index].status = "Status: not read";
@@ -126,11 +188,69 @@ function changeStatus(event, index) {
     const currentBookListItemStatus = currentBookList.querySelector(
       "[data-bookstatus]"
     );
+    const span = currentBookList.querySelector("span");
     currentBookListItemStatus.textContent = "Status: not read";
     event.target.textContent = "Mark as read";
+    span.textContent = "not read";
+    span.classList.remove("statusRead");
+    span.classList.add("statusNot");
     const storageStatus = "status" + index;
     localStorage.setItem("status" + index, "not read");
   }
+}
+
+function populateStorage(array, index) {
+  localStorage.setItem("title" + index, array[index].title);
+  localStorage.setItem("author" + index, array[index].author);
+  localStorage.setItem("pages" + index, array[index].pages);
+  localStorage.setItem("status" + index, array[index].status);
+}
+
+function localStorageCheck() {
+  if (localStorage.length > 16) {
+    const initalBookCollection = bookCollection.length;
+    const completeCollection = Number(localStorage.length / 4);
+    for (i = initalBookCollection; i < completeCollection; i++) {
+      let bookTitle = localStorage.getItem("title" + i);
+      let bookAuthor = localStorage.getItem("author" + i).slice(8);
+      let bookPages = localStorage.getItem("pages" + i).slice(7);
+      let bookStatus = localStorage.getItem("status" + i).slice(8);
+      addBookToCollection(
+        bookTitle,
+        bookAuthor,
+        bookPages,
+        bookStatus,
+        "fromstorage"
+      );
+    }
+  } else {
+    return;
+  }
+}
+
+function hideLi(titleLi) {
+  titleLi.forEach(function (title) {
+    title.addEventListener("click", function (event) {
+      const bookIndex = event.target.getAttribute("data-booktitle").slice(9);
+      const bookId = "book" + bookIndex;
+      const listUl = document.getElementById(bookId);
+      let li = listUl.childNodes;
+      li.forEach(function (element) {
+        let classes = element.classList;
+        classes.toggle("visibleClass");
+      });
+    });
+    const span = document.createElement("span");
+    const bookIndex = title.getAttribute("data-booktitle").slice(9);
+    const bookStatus = bookCollection[bookIndex].status.slice(8);
+    if (bookStatus === "read") {
+      span.classList.add("statusRead");
+    } else {
+      span.classList.add("statusNot");
+    }
+    span.textContent = bookStatus;
+    title.appendChild(span);
+  });
 }
 
 const formDiv = document.getElementById("formdiv");
@@ -144,10 +264,8 @@ const form = document.getElementById("newbook");
 const closeForm = document.getElementById("closeform");
 
 window.onload = function () {
-  displayCollection(bookCollection);
-  console.log(bookCollection);
-  localStorageCheck();
-  console.log(bookCollection);
+  const titleLi = document.querySelectorAll("[data-booktitle]");
+  hideLi(titleLi);
 };
 
 showFormButton.addEventListener("click", function (event) {
@@ -175,32 +293,5 @@ closeForm.addEventListener("click", function (event) {
   showFormDiv.style.visibility = "visible";
 });
 
-function populateStorage(array, index) {
-  localStorage.setItem("title" + index, array[index].title);
-  localStorage.setItem("author" + index, array[index].author);
-  localStorage.setItem("pages" + index, array[index].pages);
-  localStorage.setItem("status" + index, array[index].status);
-}
-
-function localStorageCheck() {
-  if (localStorage.length > 16) {
-    const initalBookCollection = bookCollection.length;
-    const completeCollection = Number(localStorage.length / 4);
-    //const newBooks = completeCollection-initalBookCollection;
-    for (i = initalBookCollection; i < completeCollection; i++) {
-      let bookTitle = localStorage.getItem("title" + i);
-      let bookAuthor = localStorage.getItem("author" + i);
-      let bookPages = localStorage.getItem("pages" + i).slice(7);
-      let bookStatus = localStorage.getItem("status" + i).slice(8);
-      addBookToCollection(
-        bookTitle,
-        bookAuthor,
-        bookPages,
-        bookStatus,
-        "fromstorage"
-      );
-    }
-  } else {
-    return;
-  }
-}
+displayCollection(bookCollection);
+localStorageCheck();
